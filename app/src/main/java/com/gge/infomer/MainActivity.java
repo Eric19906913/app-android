34a2e47f7aefceef8a817b346fd756def0c8fae8
Eric Gaston Haas <ericgaston1234@gmail.com>
2020-03-19 13:10:24 -0300

    agrego logica de botones y textos

--- app/src/main/java/com/gge/infomer/MainActivity.java
+++ app/src/main/java/com/gge/infomer/MainActivity.java
@@ -3,12 +3,36 @@ package com.gge.infomer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText EdtUser, EdtPass;
+    Button BtnLog;
+    RadioButton RbPerson, RbEnter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EdtUser = (EditText) findViewById(R.id.EdtUser);
+        EdtPass = (EditText) findViewById(R.id.EdtPass);
+        BtnLog = (Button) findViewById(R.id.BtnLog);
+        RbPerson = (RadioButton) findViewById(R.id.RbPerson);
+        RbEnter = (RadioButton) findViewById(R.id.RbEnterprise);
+    }
+
+
+    public void ingreso(View view) {
+        String nombre = EdtUser.toString();
+        if(!nombre.isEmpty()){
+            Toast.makeText(this,nombre,Toast.LENGTH_LONG);
+        }else{
+            Toast.makeText(this,"No ingreso nombre",Toast.LENGTH_LONG);
+        }
+
     }
 }
