4b6465356230b4393f67ea027590d64cafc714d9
Eric Gaston Haas <ericgaston1234@gmail.com>
2020-03-20 20:58:49 -0300

    cosas
    asdasd

--- app/src/main/java/com/gge/infomer/MainActivity.java
+++ app/src/main/java/com/gge/infomer/MainActivity.java
@@ -2,6 +2,7 @@ package com.gge.infomer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         String nombre = EdtUser.getText().toString();
         String pass = EdtPass.getText().toString();
         Boolean opcion;
+        Intent intent;
         if(CheckBoxEnterprise.isChecked()){
             opcion = true;
         }else{
@@ -37,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         if(!nombre.isEmpty() && !pass.isEmpty()){
             if(opcion){
                 Toast.makeText(this,"Ingreso correcto de empresa",Toast.LENGTH_SHORT).show();
+                intent = new Intent(MainActivity.this, EnterpriseActivity.class);
             }else {
                 Toast.makeText(this, "Ingreso correcto de usuario", Toast.LENGTH_SHORT).show();
             }
