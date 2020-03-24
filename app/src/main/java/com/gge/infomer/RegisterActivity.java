f53108ea4e15b6558f40c695a1737166a32629e8
Eric Gaston Haas <ericgaston1234@gmail.com>
2020-03-23 23:10:36 -0300

    agrego registro

---
+++ app/src/main/java/com/gge/infomer/RegisterActivity.java
@@ -0,0 +1,13 @@
+package com.gge.infomer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
